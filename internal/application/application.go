ef24837f12e8244aa2a5ccf015e4973145975242
Febriansyah <fbriansyah@gmail.com>
2023-12-02 18:10:58 +0700

    feat(user): Create App using CQRS
    - Add CreateUser Command
    - Implementing Config Pattern when creating App

--- internal/application/application.go
+++ internal/application/application.go
@@ -1,1 +1,52 @@
 package application
+
+import (
+	"context"
+
+	"github.com/fbriansyah/go-microservice/internal/application/commands"
+	"github.com/fbriansyah/go-microservice/internal/application/domain/user"
+)
+
+type (
+	App interface {
+		Commands
+	}
+	Commands interface {
+		CreateUser(ctx context.Context, cmd *commands.CreateUserCmd) error
+	}
+	Application struct {
+		userRepo user.Repository
+		appCommands
+	}
+	appCommands struct {
+		commands.CreateUserHandler
+	}
+)
+
+var _ App = (*Application)(nil)
+
+type ApplicationConfig func(*Application) error
+
+func WithUserRepo(userRepo user.Repository) ApplicationConfig {
+	return func(app *Application) error {
+		app.userRepo = userRepo
+		return nil
+	}
+}
+
+func New(cfgs ...ApplicationConfig) (*Application, error) {
+	app := &Application{}
+
+	for _, cfg := range cfgs {
+		err := cfg(app)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	app.appCommands = appCommands{
+		CreateUserHandler: commands.NewUserHandler(app.userRepo),
+	}
+
+	return app, nil
+}
---
+++ internal/application/commands/create_user.go
@@ -0,0 +1,34 @@
+package commands
+
+import (
+	"context"
+
+	"github.com/fbriansyah/go-microservice/internal/application/domain/user"
+	"github.com/fbriansyah/go-microservice/util"
+)
+
+type (
+	CreateUserCmd struct {
+		Name     string
+		Email    string
+		Password string
+	}
+	CreateUserHandler struct {
+		userRepo user.Repository
+	}
+)
+
+func NewUserHandler(userRepo user.Repository) CreateUserHandler {
+	return CreateUserHandler{
+		userRepo: userRepo,
+	}
+}
+
+func (h CreateUserHandler) CreateUser(ctx context.Context, cmd *CreateUserCmd) error {
+	password, err := util.HashPassword(cmd.Password)
+	if err != nil {
+		return err
+	}
+	user := user.NewUser(cmd.Name, cmd.Email, password)
+	return h.userRepo.Save(ctx, user)
+}
